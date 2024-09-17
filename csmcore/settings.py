c13d4bd7a27e5f9219e86b4caea5f89f75160dd8
ERFAN <erfan.shokr300@gmail.com>
2024-09-17 23:48:02 +0400

    Update Django settings and Docker configuration

--- csmcore/settings.py
+++ csmcore/settings.py
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-_(plw^z*dm-7w21@97f59j&x47q&@o1825qgf%4k3e)6cu1vjy'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
